ab4f30db8cb26ebe6bb82eee597fa96a82918f81
unknown <dhavalleelawala@gmail.com>
2025-11-25 13:02:43 +0530

    update

--- day-9-form-crud/src/App.jsx
+++ day-9-form-crud/src/App.jsx
@@ -1,104 +1,139 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function App() {
-  const [user,setUser] = useState({});
-  const [list,setList] = useState([]);
-  const [editId,setEditId] = useState(null);
-  const handleChange = (e)=>{
-    const {name,value} = e.target;
-    setUser({...user,[name]:value});
-  }
+  const [user, setUser] = useState({});
+  const [list, setList] = useState([]);
+  const [editId, setEditId] = useState(null);
+  const [error,setError] = useState({});
 
-  const handleSubmit = (e)=>{
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if(editId == null){
-      setList([...list,{...user,id:Date.now()}]);
-    }else{
-      let data = list.map((val)=>{
-        if(val.id == editId){
-          return {...val,...user};
+
+    if(validation()) {return};
+
+    if (editId == null) {
+      setList([...list, { ...user, id: Date.now() }]);
+    } else {
+      let data = list.map((val) => {
+        if (val.id == editId) {
+          return { ...val, ...user };
         }
         return val;
-      })
+      });
       setList(data);
       setEditId(null);
     }
+    
+    
     setUser({});
-  }
+  };
 
-  const handleDelete = (id)=>{
-    let data = list.filter(val => val.id != id);
+  const handleDelete = (id) => {
+    let data = list.filter((val) => val.id != id);
     setList(data);
-  }
+  };
 
-  const handleEdit = (id)=>{
-    let editData = list.find(val => val.id == id);
+  const handleEdit = (id) => {
+    let editData = list.find((val) => val.id == id);
     setUser(editData);
     setEditId(id);
+  };
+
+  const validation=()=>{
+    let error = {};
+
+    if(!user.username) error.username = `Username is Required.`;
+    if(!user.email) error.email = `Email is Required.`;
+    setError(error);
+    return Object.keys(error).length != 0;
   }
 
+  
   return (
-    <>
-      <form action="" method="post" onSubmit={handleSubmit} >
-        <div>
-          <label htmlFor="username">User Name</label>
-          <input 
-            type="text" 
-            id='username'  
-            onChange={handleChange} 
-            name='username' 
-            value={user.username || ''} 
-          />
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <form action="" method="post" onSubmit={handleSubmit}>
+            <h3>Sign Up</h3>
+            <div className="mb-3">
+              <label className="form-label" htmlFor="username">User Name</label>
+              <input
+                className="form-control"
+                type="text"
+                id="username"
+                onChange={handleChange}
+                name="username"
+                value={user.username || ""}
+              />
+              {error.username ? <span className="text-danger">{error.username}</span> : ''}
+            </div>
+            <div className="mb-3">
+              <label className="form-label" htmlFor="email">Email</label>
+              <input
+                className="form-control"
+                type="text"
+                id="email"
+                onChange={handleChange}
+                name="email"
+                value={user.email || ""}
+              />
+              {error.email ? <span className="text-danger">{error.email}</span> : ''}
+            </div>
+            <button type="submit" className="btn btn-primary">Add User Data.</button>
+          </form>
         </div>
-        <br />
-        <div>
-          <label htmlFor="email">Email</label>
-          <input 
-            type="text" 
-            id='email' 
-            onChange={handleChange} 
-            name='email' 
-            value={user.email || ''} 
-          />
-        </div> <br />
-          <button type='submit' >Add User Data.</button>
-      </form> 
-
-      <br /><br />
-
-      <table style={{"width":"50%"}} border={"2px"}>
-        <thead>
-          <tr>
-            <th>Sr No</th>
-            <th>Username</th>
-            <th>Email</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody style={{"textAlign":"center"}}>
-            {
-              list.length >0 ? 
-                list.map((value,index)=>(
-                  <tr key={value.id}>
-                    <td>{index+1}</td>
-                    <td>{value.username}</td>
-                    <td>{value.email}</td>
-                    <td>
-                      <button onClick={()=> handleDelete(value.id)}>Delete</button>
-                      <button onClick={()=> handleEdit(value.id)}>Edit</button>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <div className="table-responsive">
+            <table className="table table-bordered caption-top">
+              <caption>
+                <h2>User Data</h2>
+              </caption>
+              <thead>
+                <tr>
+                  <th>Sr No</th>
+                  <th>Username</th>
+                  <th>Email</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody style={{ textAlign: "center" }}>
+                {list.length > 0 ? (
+                  list.map((value, index) => (
+                    <tr key={value.id}>
+                      <td>{index + 1}</td>
+                      <td>{value.username}</td>
+                      <td>{value.email}</td>
+                      <td>
+                        <button className="btn btn-outline-danger me-2" onClick={() => handleDelete(value.id)}>
+                          Delete
+                        </button>
+                        <button className="btn btn-outline-warning" onClick={() => handleEdit(value.id)}>
+                          Edit
+                        </button>
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan={4}>
+                      <strong>Data Not Found</strong>
                     </td>
                   </tr>
-                ))
-              : 
-              <tr>
-                <td colSpan={4}><strong>Data Not Found</strong></td>
-              </tr>
-            }
-        </tbody>
-      </table>
-
-    </>
-  )
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
